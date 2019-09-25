2fab3110574b77715f038ef655dfa6ba173a097f
Max Mecklin <max@meckl.in>
2019-09-25 12:45:21 +0300

    Import to git

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, make_response, jsonify, request, abort
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+from modules.database import *
+import datetime
+import os
+
+
+def unauthorized():
+    """
+    Returns response for unauthorized access.
+    :return:
+    """
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+def authenticate(username, password):
+    """
+    Authenticate user if exists.
+    :param username:
+    :param password:
+    :return:
+    """
+    user = auth(username, password)
+
+    if user:
+        return User(user['id'], user['user'], user['password'])
+
+
+def identity(payload):
+    """
+    Get user from payload.
+    :param payload:
+    :return:
+    """
+    user_id = payload['identity']
+
+    return get_user_by_id(user_id)
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = get_secret()
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(hours=3)
+jwt = JWT(app, authenticate, identity)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('v1/users/add', methods=['POST'])
+@jwt_required()
+def add_user():
+    if not request.json or 'user' not in request.json:
+        abort(400)
+
+    user = request.json['user']
+
+    if 'username' not in user or 'password' not in user:
+        abort(400)
+
+    return jsonify(create_user(user['username'], user['password']))
+
+
+@app.route('v1/users/edit', methods=['POST'])
+@jwt_required()
+def edit_user():
+    if not request.json or 'user' not in request.json:
+        abort(400)
+
+    user = request.json['user']
+
+    if 'username' not in user or 'password' not in user or 'id' not in user:
+        abort(400)
+
+    return jsonify(update_user(user['id'], user['username'], user['password']))
+
+
+@app.route('v1/files/<path:p>', methods=['GET, POST'])
+@jwt_required()
+def files(p):
+    root_dir = config['root-dir']
+    path = os.path.join(root_dir, p)
+    if request.method == "GET":
+        if os.path.isdir(path):
+            contents = {}
+            for f in os.listdir(path):
+                if os.path.isfile(os.path.join(path, f)):
+                    contents[f] = "file"
+                else:
+                    contents[f] = "directory"
+            return jsonify({"Status": "directory", "data": {"dir": contents}})
+        else:
+            with open('r', path) as rf:
+                return jsonify({"Status": "file", "data": {'filename': path, 'contents': rf.read()}})
+
+    else:
+        if not request.json or 'contents' not in request.json:
+            abort(400)
+
+        if os.path.isfile(path):
+            with open('w', path) as wf:
+                wf.write(request.json['contents'])
+
+            return jsonify({'Status': 'success', "data": {'filename': path, 'contents': request.json['contents']}})
+        return jsonify({"Status": "error", "error": "Post is not allowed for directories."})
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ modules/database.py
@@ -0,0 +1,122 @@
+import pymysql
+import json
+
+
+def get_conf():
+    with open('r', 'config.json') as conf_file:
+        return json.load(conf_file)
+
+
+def get_secret():
+    """
+    Get secret from config.
+    :return:
+    """
+    with open("config/config.json", "r") as conf:
+        data = json.load(conf)
+
+    return data['secret']
+
+
+# Work around for working with Flask-JWT.
+class User:
+    def __init__(self, user_id, username, access_token):
+        self.id = user_id
+        self.username = username
+        self.access_token = access_token
+
+
+config = get_conf()
+
+
+def insert(sql, values=None):
+    """
+    Insert data into database.
+    :param sql:
+    :param values:
+    :return:
+    """
+    db = pymysql.connect(
+        config['host'],
+        config['username'],
+        config['password'],
+        config['name'],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    try:
+        with db.cursor() as cursor:
+            if values is not None:
+                cursor.execute(sql, values)
+            else:
+                cursor.execute(sql)
+
+        db.commit()
+        db.close()
+        return True
+    except pymysql.MySQLError as e:
+        db.rollback()
+        db.close()
+        print(e, e.args)
+        return False
+
+
+def get(sql, values=None, everything=True):
+    """
+    Get data from database.
+    :param sql:
+    :param values:
+    :param everything:
+    :return:
+    """
+    db = pymysql.connect(
+        config['host'],
+        config['username'],
+        config['password'],
+        config['name'],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    try:
+        with db.cursor() as cursor:
+            if values is not None:
+                cursor.execute(sql, values)
+            else:
+                cursor.execute(sql)
+
+            if everything:
+                data = cursor.fetchall()
+            else:
+                data = cursor.fetchone()
+
+        db.close()
+        return data
+    except pymysql.MySQLError:
+        db.close()
+        return None
+
+
+def auth(username: str, password: str):
+    sql = "SELECT * FROM `users` WHERE `username`=%s AND `password`=ENCRYPT(%s, `password`);"
+    return get(sql, (username, password), False)
+
+
+def get_user_by_id(user_id):
+    sql = "SELECT * FROM `users` WHERE `id`=%s;"
+    return get(sql, (user_id,), False)
+
+
+def create_user(username: str, password: str):
+    sql = "INSERT INTO `users` (`username`, `password`) VALUES (%s, ENCRYPT(%s, CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))));"
+    if insert(sql, (username, password)):
+        return {'Status': 'success'}
+    else:
+        return {'Status': 'Error, unable to create user.'}
+
+
+def update_user(id, username: str, password: str):
+    sql = "UPDATE `users` SET `password`=ENCRYPT('%s', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), `username`=%s WHERE `id`='%s';"
+    if insert(sql, (password, username, id)):
+        return {'Status': 'success'}
+    else:
+        return {'Status': 'Error, unable to update user.'}
